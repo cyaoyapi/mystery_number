d540117442e327b895298277d5d71c81d36857ff
Cyrille YAO <cyaoyapi@gmail.com>
2018-07-18 02:40:54 +0200

    Version 1.0 of the game

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    /*
+        Mystery number randomly generation
+    */
+    const int MAX = 100, MIN = 1;
+    srand(time(NULL));
+    int mystery_number = (rand() % (MAX - MIN + 1)) + MIN;
+
+    int gamer_choice = 0;
+
+
+    /*
+        Game over
+    */
+    int cpt = 1 ;
+    while(cpt){
+        printf("Quel est le nombre mystère ? (Entre %d et %d)\n", MIN, MAX);
+        scanf("%d", &gamer_choice);
+        if(gamer_choice < MIN || gamer_choice > MAX){
+            printf("Erreur de saisi : entrez un entier entre %d et %d\n", MIN, MAX);
+        }
+        else if(gamer_choice > mystery_number){
+            printf("C'est moins\n");
+        }
+        else if(gamer_choice < mystery_number){
+            printf("C'est plus\n");
+        }
+        else{
+            cpt = 0;
+        }
+    }
+    printf("Bravo, vous avez trouvez le nombre mystère !\n");
+
     return 0;
 }
